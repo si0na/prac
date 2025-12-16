885db0ee9b449a59ef2a4db2e968d9fbe48520c2
Siona Mariam Thomas <siona.thomas@reflectionsinfos.com>
2025-12-17 02:31:01 +0530

    new branch

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -15,7 +15,7 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
+    it('it returns users data', () => {
       expect(appController.getHello()).toBe('Hello World!');
     });
   });
